3925c626d7d2c1b6ab43c39431c5ee70ff56680f
yamne-it <61476433+yamne-it@users.noreply.github.com>
2021-01-19 14:44:05 +0100

    completed initial server side

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100
     const publishableKey = 'pk_test_51HqkgzKpAjLUtQVoD4xQ8TWNAreIt0uRze6v8Uw5juD3MOjkFvwImnGisUboW3D2L6fabWxwoiqDv7C6FP64j6dG00R6UtiHgF'
 
     const onToken = (token) => {
-        console.log(token)
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => { 
+          alert('Payment successful')
+        }).catch(error => {
+          console.log('Payment error:', JSON.parse(error))
+          alert('There was an issu with your payment. Please sure you use the provided credit card.')
+        })
+        // console.log(token)
+        // alert('Payment Successful')
     }
 
     const stripeCheckoutButton = (
